5770e13e6244b560c62e0831028b68d2d4415346
AayanBaig-WhitehatJR <71639274+AayanBaig-WhitehatJR@users.noreply.github.com>
2021-01-09 19:14:17 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -1,25 +1,17 @@
-var username = localStorage.getItem("Player Name")
-var mode = localStorage.getItem("Mode:")
-if(mode == "Easy"){
-function displayQuestion(){
-    document.getElementById("output").innerHTML = "20x1"
-       answer = 20
-}}
-if(mode == "Medium"){
-    function displayQuestion(){
-        document.getElementById("output").innerHTML = "17/8"
-        answer = 2.125
-    }
-}
-if(mode == "Hard"){
-    function displayQuestion(){
-        document.getElementById("output").innerHTML = "2^5"
-        answer = 32
-    }
-}
-if(mode == "Hardcore"){
-    function displayQuestion(){
-        document.getElementById("output").innerHTML = "2^5x3/2"
-        answer = 48
-    }
+var username1 = localStorage.getItem("Player Name")
+var username2 = localStorage.getItem("Player Name 2")
+
+var questionTurn = document.getElementById("questionTurn").innerHTML = "Question Turn: " + username1;
+var answerTurn = document.getElementById("answerTurn").innerHTML = "Answer Turn: " + username2;
+function send(){    
+var n1 = document.getElementById("inputN1").value
+var n2 = document.getElementById("inputN2").value 
+answer = parseInt(n1) * parseInt(n2)
+question_number = "<h4>" + n1 + "X" + n2 + "</h4>"
+input_box = "<br>Answer: <input type = 'text' id = 'inputChkBox'>"
+checkSumButton = "<br><br><button class = 'btn btn-info' onclick ='check()'>Check</button>"
+row = question_number + input_box + checkSumButton
+document.getElementById("output").innerHTML = row
+document.getElementById("inputN1").value = ""
+document.getElementById("inputN2").value = ""
 }
